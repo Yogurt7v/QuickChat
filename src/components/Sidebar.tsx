669198f8089dac0e424d5ddd55f00623ceb26f15
Yogurt7v <egorov-2k@yandex.ru>
2025-10-14 21:18:26 +0300

    добавлен выход

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,22 @@
 import styles from '../styles/Sidebar.module.css';
 import ChatItem from './ChatItem';
 import { useChatStore } from '../store/chatStore';
+import exit from '../assets/exit.svg';
+import { useAuthStore } from '../store/authStore';
 
 export default function Sidebar() {
   const { chats, selectedChat, selectChat } = useChatStore();
+  const { logout } = useAuthStore();
 
   return (
     <aside className={styles.container}>
       <div className={styles.header}>
-        <h2 className={styles.title}>Чаты</h2>
+        <div className={styles.headerTop}>
+          <h2 className={styles.title}>Чаты</h2>
+          <button className={styles.exitButton} onClick={logout}>
+            <img src={exit} />
+          </button>
+        </div>
         <input type="text" placeholder="Поиск" className={styles.searchInput} />
       </div>
       <div className={styles.chatList}>
--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,12 +1,36 @@
 import { create } from 'zustand';
 import type { User } from '../types';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase/config';
 
 interface AuthState {
   user: User | null;
   setStoreUser: (user: User | null) => void;
+  logout: () => void;
 }
 
+const loadUserFromStorage = (): User | null => {
+  try {
+    const stored = localStorage.getItem('quickchat');
+    return stored ? JSON.parse(stored) : null;
+  } catch {
+    return null;
+  }
+};
+
 export const useAuthStore = create<AuthState>(set => ({
-  user: null,
-  setStoreUser: user => set({ user }),
+  user: loadUserFromStorage(),
+  setStoreUser: user => {
+    if (user) {
+      localStorage.setItem('quickchat', JSON.stringify(user));
+    } else {
+      localStorage.removeItem('quickchat');
+    }
+    set({ user });
+  },
+  logout: () => {
+    signOut(auth); // Выход из Firebase
+    set({ user: null });
+    localStorage.removeItem('quickchat');
+  },
 }));

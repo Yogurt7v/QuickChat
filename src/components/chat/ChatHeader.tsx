e85424a2e42a33ca0fe0446c07e825ee468e8ba8
Yogurt7v <egorov-2k@yandex.ru>
2025-12-04 21:21:06 +0300

    добавлена кнопка пересылки файлов. пока не работает

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -1,6 +1,7 @@
 import { useChatStore } from '../../store/chatStore';
 import { useIsMobile } from '../../hooks/useIsMobile';
-import back from '../../assets/back.svg';
+import back from '../../assets/Back.svg';
+import paperClip from '../../assets/Paperclip.svg';
 import styles from '../../styles/ChatArea.module.css';
 
 interface ChatHeaderProps {
@@ -17,30 +18,50 @@ export default function ChatHeader({
   const { clearSelectedChat } = useChatStore();
   const isMobile = useIsMobile();
 
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      console.log('Uploaded file:', files[0]);
+    }
+  };
+
   return (
     <header className={styles.newHeader}>
-      <div className={styles.newHeaderContainer}>
-        {isMobile && (
-          <button
-            className={styles.roundButton}
-            onClick={clearSelectedChat}
-            aria-label="Назад"
-          >
-            <img className={styles.svg} src={back} alt="Назад" />
-          </button>
-        )}
+      <div className={styles.newHeaderContent}>
+        <div className={styles.newHeaderContainer}>
+          {isMobile && (
+            <button
+              className={styles.roundButton}
+              onClick={clearSelectedChat}
+              aria-label="Назад"
+            >
+              <img className={styles.svg} src={back} alt="Назад" />
+            </button>
+          )}
 
-        <div className={styles.headerInfo}>
-          <div className={styles.headerTopRow}>
-            <h2 className={styles.chatTitle}>{chatPartnerName}</h2>
-          </div>
+          <div className={styles.headerInfo}>
+            <div className={styles.headerTopRow}>
+              <h2 className={styles.chatTitle}>{chatPartnerName}</h2>
+            </div>
 
-          <div className={styles.headerBottomRow}>
-            <span className={styles.statusText}>
-              {typingDisplay || chatStatus}
-            </span>
+            <div className={styles.headerBottomRow}>
+              <span className={styles.statusText}>
+                {typingDisplay || chatStatus}
+              </span>
+            </div>
           </div>
         </div>
+        <button className={styles.roundButton} aria-label="Прикрепить файл">
+          <label htmlFor="fileInput">
+            <img className={styles.svg} src={paperClip} alt="inputFile" />
+          </label>
+          <input
+            id="fileInput"
+            type="file"
+            onChange={handleFileUpload}
+            className={styles.inputFile}
+          />
+        </button>
       </div>
     </header>
   );

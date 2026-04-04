822c4465155273b0827780427eef5ac52201fbdc
Yogurt7v <egorov-2k@yandex.ru>
2026-04-04 18:09:10 +0300

    небольшая правка стилей

--- src/components/chat/ChatPlaceholder.tsx
+++ src/components/chat/ChatPlaceholder.tsx
@@ -3,7 +3,7 @@ import styles from '../../styles/ChatPlaceholder.module.css';
 export default function ChatPlaceholder() {
   return (
     <div className={styles.placeholder}>
-      <h3>Выберите чат, чтобы начать общение</h3>
+      <h3>Выберите чат, или добавьте собеседника для общения. </h3>
     </div>
   );
 }

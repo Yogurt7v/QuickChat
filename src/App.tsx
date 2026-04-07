41cf3870be8f4b275d03af9e2bcf43f05b30ac6b
Yogurt7v <egorov-2k@yandex.ru>
2026-04-07 20:18:24 +0300

    тоже лишнее

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { useAuthStore } from './store/authStore';
 import { useAuth } from './hooks/useAuth';
 import { useOnlineStatus } from './hooks/useOnlineStatus';
 import { useEffect } from 'react';
-import { NotificationPermissionBanner } from './components/ui/NotificationPermissionBanner';
 import { usePushNotifications } from './hooks/usePushNotifications';
 import { VAPID_PUBLIC_KEY } from './constants';
 import ErrorBoundary from './components/ui/ErrorBoundary';
@@ -23,9 +22,8 @@ function AppContent() {
     if ('serviceWorker' in navigator) {
       const registerSW = async () => {
         try {
-          const registration = await navigator.serviceWorker.register(
-            '/service-worker.js'
-          );
+          const registration =
+            await navigator.serviceWorker.register('/service-worker.js');
           console.log('✅ SW зарегистрирован:', registration.scope);
         } catch (err) {
           console.error('❌ Ошибка регистрации SW:', err);
@@ -38,7 +36,6 @@ function AppContent() {
   return (
     <>
       {user ? <Layout /> : <LoginForm />}
-      {user && <NotificationPermissionBanner />}
       <ConnectionBanner />
     </>
   );
